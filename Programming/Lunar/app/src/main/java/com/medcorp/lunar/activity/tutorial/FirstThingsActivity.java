545f89b44cddbc26cfb644dc1f8f5a8595e486e9
jason <xiongcai@med-corp.net>
2017-09-04 17:58:36 +0800

    [complete the first things]

--- Programming/Lunar/app/src/main/java/com/medcorp/lunar/activity/tutorial/FirstThingsActivity.java
+++ Programming/Lunar/app/src/main/java/com/medcorp/lunar/activity/tutorial/FirstThingsActivity.java
@@ -9,8 +9,8 @@ import android.os.Bundle;
 import android.provider.Settings;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
-import android.util.Log;
-import android.widget.ImageButton;
+import android.support.v4.app.ActivityCompat;
+import android.widget.ImageView;
 
 import com.afollestad.materialdialogs.DialogAction;
 import com.afollestad.materialdialogs.MaterialDialog;
@@ -34,83 +34,117 @@ import butterknife.OnClick;
 public class FirstThingsActivity extends BaseActivity {
 
     @Bind(R.id.first_things_access_notification)
-    ImageButton notificationButton;
+    ImageView notificationButton;
     @Bind(R.id.first_things_access_location)
-    ImageButton locationButton;
+    ImageView locationButton;
     @Bind(R.id.first_things_access_google_fit)
-    ImageButton googleFitButton;
+    ImageView googleFitButton;
+    private PackageManager mPackageManager;
+    private String mPackageName;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.first_things_activity);
         ButterKnife.bind(this);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
         initView();
     }
 
     private void initView() {
-        PackageManager packageManager = getPackageManager();
-        boolean coarseLocation = PackageManager.PERMISSION_GRANTED == packageManager.checkPermission("android.permission.ACCESS_COARSE_LOCATION", getPackageName());
-        boolean fineLocation = PackageManager.PERMISSION_GRANTED == packageManager.checkPermission("android.permission.ACCESS_FINE_LOCATION", getPackageName());
+        mPackageName = getPackageName();
+        mPackageManager = getPackageManager();
+        boolean coarseLocation = PackageManager.PERMISSION_GRANTED ==
+                mPackageManager.checkPermission("android.permission.ACCESS_COARSE_LOCATION", mPackageName);
+        boolean fineLocation = PackageManager.PERMISSION_GRANTED ==
+                mPackageManager.checkPermission("android.permission.ACCESS_FINE_LOCATION", mPackageName);
         if (coarseLocation == true && fineLocation == true) {
-            locationButton.setImageDrawable(getResources().getDrawable(R.drawable.ic_ais_error));
-        } else {
             locationButton.setImageDrawable(getResources().getDrawable(R.drawable.ic_right_icon));
+        } else {
+            locationButton.setImageDrawable(getResources().getDrawable(R.drawable.ic_ais_error));
         }
 
         ContentResolver contentResolver = this.getContentResolver();
         String enabledNotificationListeners = Settings.Secure.getString(contentResolver, "enabled_notification_listeners");
-        String packageName = getPackageName();
-        if (enabledNotificationListeners == null || !enabledNotificationListeners.contains(packageName)) {
+        if (enabledNotificationListeners == null || !enabledNotificationListeners.contains(mPackageName)) {
             notificationButton.setImageDrawable(getResources().getDrawable(R.drawable.ic_ais_error));
         } else {
             notificationButton.setImageDrawable(getResources().getDrawable(R.drawable.ic_right_icon));
         }
+
+        if (!Preferences.isGoogleFitSet(this)) {
+            googleFitButton.setImageDrawable(getResources().getDrawable(R.drawable.ic_ais_error));
+        } else {
+            googleFitButton.setImageDrawable(getResources().getDrawable(R.drawable.ic_right_icon));
+        }
     }
 
-    @OnClick(R.id.first_things_access_notification)
+    @OnClick(R.id.first_things_access_notification_rl)
     public void accessNotifications() {
         getNotificationAccessPermission(this);
     }
 
-    @OnClick(R.id.first_things_access_location)
+    @OnClick(R.id.first_things_access_location_rl)
     public void accessLocation() {
         PermissionRequestDialogBuilder builder = new PermissionRequestDialogBuilder(this);
         builder.addPermission(Manifest.permission.ACCESS_COARSE_LOCATION);
         builder.addPermission(Manifest.permission.ACCESS_FINE_LOCATION);
         builder.setText(R.string.location_access_content);
         builder.setTitle(R.string.location_access_title);
-        builder.askForPermission(this, 1);
+        builder.askForPermission(this, 0x08);
     }
 
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        if (requestCode == 1) {
-            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                Log.d("TutorialPage4Activity", "request location permission success.");
-                EventBus.getDefault().post(new SetSunriseAndSunsetTimeRequestEvent(SetSunriseAndSunsetTimeRequestEvent.STATUS.START));
-            } else {
-                Log.w("TutorialPage4Activity", "request location permission failed.");
-                new MaterialDialog.Builder(this)
-                        .title(android.R.string.dialog_alert_title)
-                        .content(R.string.permission_location_message)
-                        .onPositive(new MaterialDialog.SingleButtonCallback() {
-                            @Override
-                            public void onClick(MaterialDialog dialog, DialogAction which) {
-                                notificationButton.setImageDrawable(getResources().getDrawable(R.drawable.ic_right_icon));
-                            }
-                        })
-                        .negativeText(android.R.string.ok)
-                        .cancelable(false)
-                        .show();
+        if (requestCode == 0x08) {
+            if (permissions[0].equals(Manifest.permission.ACCESS_COARSE_LOCATION)
+                    && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                if (ActivityCompat.checkSelfPermission(getBaseContext(),
+                        Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                        && ActivityCompat.checkSelfPermission(getBaseContext()
+                        , Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                    new MaterialDialog.Builder(this)
+                            .title(android.R.string.dialog_alert_title)
+                            .content(R.string.permission_location_message)
+                            .onNegative(new MaterialDialog.SingleButtonCallback() {
+                                @Override
+                                public void onClick(MaterialDialog dialog, DialogAction which) {
+                                    finish();
+                                }
+                            })
+                            .negativeText(android.R.string.ok)
+                            .cancelable(false)
+                            .show();
+                    locationButton.setImageDrawable(getResources().getDrawable(R.drawable.ic_ais_error));
+                } else {
+                    EventBus.getDefault().post(new SetSunriseAndSunsetTimeRequestEvent(SetSunriseAndSunsetTimeRequestEvent.STATUS.START));
+                    locationButton.setImageDrawable(getResources().getDrawable(R.drawable.ic_right_icon));
+                }
             }
         }
     }
 
-    @OnClick(R.id.first_things_access_google_fit)
+    @OnClick(R.id.first_things_access_google_fit_rl)
     public void accessGoogleFit() {
+        new MaterialDialog.Builder(this)
+                .title(getString(R.string.first_access_google_fit))
+                .content(R.string.access_google_fit_message)
+                .positiveText(R.string.notification_ok)
+                .negativeText(R.string.cancel_update)
+                .onPositive(new MaterialDialog.SingleButtonCallback() {
+                    @Override
+                    public void onClick(@NonNull MaterialDialog dialog, @NonNull DialogAction which) {
+                        Preferences.setGoogleFit(FirstThingsActivity.this, true);
+                        getModel().initGoogleFit(FirstThingsActivity.this);
+                        googleFitButton.setImageDrawable(getResources().getDrawable(R.drawable.ic_right_icon));
 
+                    }
+                }).show();
     }
 
     public void getNotificationAccessPermission(final Context ctx) {
@@ -124,24 +158,9 @@ public class FirstThingsActivity extends BaseActivity {
                     public void onClick(MaterialDialog materialDialog, DialogAction dialogAction) {
                         Intent intent = new Intent("android.settings.ACTION_NOTIFICATION_LISTENER_SETTINGS");
                         intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-                        startActivityForResult(intent, 0x06 << 12);
+                        startActivity(intent);
                     }
                 }).show();
-    }
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode == 0x06 << 12) {
-            ContentResolver contentResolver = this.getContentResolver();
-            String enabledNotificationListeners = Settings.Secure.getString(contentResolver, "enabled_notification_listeners");
-            String packageName = getPackageName();
-            if (enabledNotificationListeners == null || !enabledNotificationListeners.contains(packageName)) {
-                notificationButton.setImageDrawable(getResources().getDrawable(R.drawable.ic_ais_error));
-            } else {
-                notificationButton.setImageDrawable(getResources().getDrawable(R.drawable.ic_right_icon));
-            }
-        }
     }
 
     @OnClick(R.id.first_things_start_button)
